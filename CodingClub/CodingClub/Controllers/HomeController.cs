6ed7891fb9c2ab8db0e72024f4c2998c7b0f78ec
Volcarion <allegoricsnake@msn.com>
2018-02-03 22:59:57 -0600

    Update HomeController.cs

--- CodingClub/CodingClub/Controllers/HomeController.cs
+++ CodingClub/CodingClub/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace QuizProgramMVC.Controllers
+namespace CodingClub.Controllers
 {
     public class HomeController : Controller
     {
